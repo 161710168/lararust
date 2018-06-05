4ce3a52a243b5d2f46330aadd27f00d78cc47d10
161710168 <161710168@smkassalaambandung.sch.id>
2018-06-05 15:18:27 +0300

    lararust

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,18 @@
+<ul class="nav nav-tabs nav-stacked">
+				<li class="active">
+				<a href="{{ url('/home') }}"><font size="6" color="magenta" face="AR DECODE">Home
+				</font></a></li>
+
+				<li><a href="{{ route('siswa.index') }}"><font size="6" color="magenta" face="AR DECODE">Siswa
+				</font></a></li>
+
+                <li><a href="{{ route('kelas.index') }}"><font size="6" color="magenta" face="AR DECODE">Kelas
+                </font></a></li>
+
+                <li><a href="{{ route('absensi.index') }}"><font size="6" color="magenta" face="AR DECODE">Absensi
+                </font></a></li>
+
+                <li><a href="{{ route('keterangan.index') }}"><font size="6" color="magenta" face="AR DECODE">Keterangan
+                </font></a></li>
+</ul>
+	
